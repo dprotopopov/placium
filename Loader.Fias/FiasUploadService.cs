f30ec675c6b9e5772007e1b0534b46209a17c7eb
protopopovdy <protopopov@narod.ru>
2021-04-02 18:03:11 +0300

    wip

--- Loader.Fias/FiasUploadService.cs
+++ Loader.Fias/FiasUploadService.cs
@@ -268,7 +268,7 @@ namespace Loader.Fias
                 },
                 new[]
                 {
-                    $"SELECT CONCAT('CREATE INDEX ON ', table_name, ' (', column_name, ');') FROM information_schema.columns WHERE table_schema = 'public' AND (column_name like '%guid' OR column_name like '%status' or column_name in ('shortname')) AND table_name IN ({string.Join(",", tableNames.Select(x => $"'{x}'"))})",
+                    $"SELECT CONCAT('CREATE INDEX ON ', table_name, ' (', column_name, ');') FROM information_schema.columns WHERE table_schema = 'public' AND column_name like ANY(ARRAY['%guid','%status','%level','shortname','scname']) AND table_name IN ({string.Join(",", tableNames.Select(x => $"'{x}'"))})",
                     $"SELECT CONCAT('CREATE UNIQUE INDEX ON ', table_name, ' (', column_name, ');') FROM information_schema.columns WHERE table_schema = 'public' AND column_name in ('record_number','record_id') AND table_name IN ({string.Join(",", tableNames.Select(x => $"'{x}'"))})"
                 }
             };
