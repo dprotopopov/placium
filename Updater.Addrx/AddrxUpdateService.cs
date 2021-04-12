e397cdea351693a1a0e1649c99a8785fad69b842
protopopovdy <protopopov@narod.ru>
2021-04-12 11:19:12 +0300

    wip

--- Updater.Addrx/AddrxUpdateService.cs
+++ Updater.Addrx/AddrxUpdateService.cs
@@ -79,13 +79,11 @@ namespace Updater.Addrx
                                         SELECT id, hstore(array_agg(key), array_agg(val)) as tags
                                         FROM (SELECT c.id, unnest(akeys(p.tags)) as key, unnest(avals(p.tags)) as val FROM placex c
                                         JOIN placex p ON c.location&&p.location
-									    AND ST_Intersects(c.location,p.location)
                                         AND c.location@p.location
                                         AND ST_Within(c.location,p.location)
                                         WHERE c.id=ANY(@ids) AND p.tags?|@keys UNION ALL
                                         SELECT c.id, concat('addr:',p.tags->'place') as key, p.tags->'name' as val FROM placex c
                                         JOIN placex p ON c.location&&p.location
-									    AND ST_Intersects(c.location,p.location)
                                         AND c.location@p.location
                                         AND ST_Within(c.location,p.location)
                                         WHERE c.id=ANY(@ids) AND p.tags?'place') as q WHERE key like 'addr%' GROUP BY id
