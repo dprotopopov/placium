e67c47636ebddce28e29fe563651707b34483d64
Dmitry Protopopov <protopopov@narod.ru>
2022-05-14 23:15:00 +0300

    Distinct(StringComparer.InvariantCultureIgnoreCase)

--- Placium.WebApi/Controllers/MapController.cs
+++ Placium.WebApi/Controllers/MapController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Placium.Services;
@@ -19,11 +20,13 @@ namespace Placium.WebApi.Controllers
         [HttpGet]
         [ProducesResponseType(200, Type = typeof(string))]
         public async Task<IActionResult> GetMap(string name = "Moscow",
-            string keys = "R446092,R162903,R226149,R1252558,R1278703,R1282181,R1304596,R1320234,R2162196,R2263058,R1988678",
+            string keys =
+                "R446092,R162903,R226149,R1252558,R1278703,R1282181,R1304596,R1320234,R2162196,R2263058,R1988678",
             int width = 1200, int height = 800)
         {
-            return Ok(await _mapService.GetMap(name, keys.Split(',').Select(x => x.Trim()).ToList(), width,
-                height));
+            return Ok(await _mapService.GetMap(name,
+                keys.Split(',').Select(x => x.Trim()).Distinct(StringComparer.InvariantCultureIgnoreCase).ToList(),
+                width, height));
         }
     }
 }
